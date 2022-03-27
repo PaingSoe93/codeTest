39c100ba632c1d6bc0c8b029a29a6264e6acdec4
Paing Aung <paingsoeaung.93@gmail.com>
2022-03-27 12:00:08 +0630

    questionone add

---
+++ questionone.js
@@ -0,0 +1,11 @@
+let textArr = ['AMOR', 'XISELA', 'JAMON', 'ROMA', 'OMAR', 'MORA','ESPONJA','RAMO', 'JAPONES', 'ARMO', 'MOJAN', 'MARO','ORAM','MONJA', 'ALEXIS'];
+
+function groupText(arrText){
+  for(i=0; i<arrText.length; i++){
+    for(j=i+1; j<arrText.length; j++){
+      
+    }
+  }
+}
+
+groupText(textArr);
